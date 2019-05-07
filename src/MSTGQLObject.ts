267b713a7278f233dd8b8a52724a6a335666df20
Michel Weststrate <mweststrate@gmail.com>
2019-05-07 12:22:46 +0200

    introduced some untested convenience methods on MSTGQLObject

--- src/MSTGQLObject.ts
+++ src/MSTGQLObject.ts
@@ -1,4 +1,5 @@
-import { types, getParent } from "mobx-state-tree"
+import { types, getParent, getSnapshot, addDisposer } from "mobx-state-tree"
+import { autorun } from "mobx"
 
 import { MSTGQLStore } from "./MSTGQLStore"
 
@@ -12,3 +13,37 @@ export const MSTGQLObject = types
       return getParent(self, 2)
     }
   }))
+  .actions(self => {
+    function getMutationParams(): string {
+      const snapshot: any = getSnapshot(self)
+      return Object.keys(snapshot)
+        .map(key => `${key}: ${JSON.stringify(snapshot[key])}`)
+        .join(", ")
+    }
+
+    return {
+      getMutationParams,
+      delete(mutationName: string) {
+        return self.store.mutate(
+          `mutation ${mutationName}(id: $id){}`,
+          { id: self.id },
+          () => {
+            getParent<any>(self).delete(self.id)
+          }
+        )
+      },
+      save(mutationName: string) {
+        return self.store.mutate(
+          `mutation ${mutationName}(${getMutationParams()}){}`
+        )
+      },
+      startAutoSave(mutationName: string) {
+        addDisposer(
+          self,
+          autorun(() => {
+            ;(self as any).save(mutationName)
+          })
+        ) // TODO: debounce
+      }
+    }
+  })
