e79f04e6c1eab9c077e4987d2c0b554496fe9b78
Michel Weststrate <mweststrate@gmail.com>
2019-05-17 09:53:12 +0200

    Killed `prevData` for now, added raw and policy options to Query

--- src/react.tsx
+++ src/react.tsx
@@ -3,7 +3,7 @@ import { useState, useContext, useRef, useCallback } from "react"
 import { observer } from "mobx-react"
 import { DocumentNode } from "graphql"
 
-import { Query } from "./Query"
+import { Query, FetchPolicy } from "./Query"
 import { MSTGQLStore } from "./MSTGQLStore"
 
 export type QueryLike<STORE, DATA> =
@@ -16,12 +16,14 @@ export type QueryProps<STORE, DATA> = {
   store?: STORE
   query?: QueryLike<STORE, DATA>
   variables?: any
+  fetchPolicy?: FetchPolicy
+  raw?: boolean
   children: (args: {
     store: STORE
     loading: boolean
     error: any
     data: DATA | undefined
-    prevData: DATA | undefined // set of previously fetched values, in case the query was replaced
+    // prevData: DATA | undefined // set of previously fetched values, in case the query was replaced
     query: Query<DATA> | undefined
     setQuery: (query: QueryLike<STORE, DATA>) => void
   }) => React.ReactElement
@@ -33,12 +35,20 @@ export function createStoreContext<STORE extends typeof MSTGQLStore.Type>() {
 
 function normalizeQuery<STORE extends typeof MSTGQLStore.Type, DATA>(
   store: STORE,
-  variables: any,
-  query: QueryLike<STORE, DATA>
+  query: QueryLike<STORE, DATA>,
+  {
+    variables,
+    fetchPolicy = "cache-and-network",
+    raw = false
+  }: {
+    variables?: any
+    fetchPolicy?: FetchPolicy
+    raw?: boolean
+  }
 ): Query<DATA> {
   if (typeof query === "function") return query(store)
   if (query instanceof Query) return query
-  return store.query(query, variables)
+  return store.query(query, variables, { fetchPolicy, raw })
 }
 
 export function createQueryComponent<STORE extends typeof MSTGQLStore.Type>(
@@ -47,30 +57,30 @@ export function createQueryComponent<STORE extends typeof MSTGQLStore.Type>(
   // TODO: it would be great to infer DATA from the props.query property, but saddly, we cannot infer the .children prop from the .query prop atm
   return observer(function Query<DATA = any>(props: QueryProps<STORE, DATA>) {
     const store = props.store || useContext(context)
-    const prevData = useRef<DATA>()
+    // const prevData = useRef<DATA>() // TODO: is this useful?
     const [query, setQuery] = useState<Query<DATA> | undefined>(() => {
       if (!props.query) return undefined
-      return normalizeQuery<STORE, DATA>(store, props.variables, props.query)
+      return normalizeQuery<STORE, DATA>(store, props.query, props)
     })
 
     const setQueryHelper = useCallback((newQuery: QueryLike<STORE, DATA>) => {
       // if the current query had results already, save it in prevData
-      if (query && query.data) prevData.current = query.data
-      setQuery(normalizeQuery(store, props.variables, newQuery))
+      // if (query && query.data) prevData.current = query.data
+      setQuery(normalizeQuery(store, newQuery, props))
     }, [])
 
     // if new query or variables are passed in, replace the query!
     React.useEffect(() => {
       if (!props.query || typeof props.query === "function") return // ignore changes to initializer func
       setQueryHelper(props.query)
-    }, [props.query, props.variables]) // TODO: props.variables should be checked on shallow-equality!
+    }, [props.query, props.raw, props.fetchPolicy, props.variables]) // TODO: props.variables should be checked on shallow-equality!
 
     return props.children({
       store,
       loading: query ? query.loading : false,
       error: query && query.error,
       data: query && query.data,
-      prevData: prevData.current,
+      // prevData: prevData.current,
       query,
       setQuery: setQueryHelper
     })
