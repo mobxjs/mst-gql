f002ecedf71f30bb7ac3004c4f059580278bf93b
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-09-18 11:05:52 -0400

    [5-nextjs] Add other page to allow client-side rendering index page

---
+++ examples/5-nextjs/pages/other.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Other() {
+  return (
+    <Link href='/'>
+      <a>Navigate to index page with fully client-side rendering</a>
+    </Link>
+  )
+}
