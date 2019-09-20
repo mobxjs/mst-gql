bc7da9b6c38be7f2c237d725e99df435523b1611
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-09-19 22:14:32 -0400

    Fixup 00a836, in deflate(), only remove props of *root* type objects

--- src/deflateHelper.ts
+++ src/deflateHelper.ts
@@ -5,12 +5,11 @@ export function deflateHelper(store: any, data: any) {
 
     const { __typename, id } = data
 
-    // GQL object
-    if (__typename && store.isKnownType(__typename)) {
-      // GQL object with known type, keep only __typename & id
+    if (__typename && store.isRootType(__typename)) {
+      // GQL object with root type, keep only __typename & id
       return { __typename, id }
     } else {
-      // GQL object with unknown type, return object with all props deflated
+      // GQL object with non-root type, return object with all props deflated
       const snapshot: any = {}
       for (const key in data) {
         snapshot[key] = deflate(data[key])
