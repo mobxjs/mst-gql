872ae684852c770bdd9f9d1a1c9ef977a896c4bd
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-09-25 03:22:06 -0400

    [5-nextjs] Demonstrate customization of ModelBase

---
+++ examples/5-nextjs/src/models/ModelBase.ts
@@ -0,0 +1,7 @@
+import { MSTGQLObject } from "mst-gql"
+import { types } from "mobx-state-tree"
+
+export const ModelBase = MSTGQLObject
+  .props({
+    $created: types.optional(types.Date, () => new Date())
+  })
