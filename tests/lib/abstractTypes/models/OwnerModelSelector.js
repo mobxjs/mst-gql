5f24785618351220e446804c99ec318305f033b8
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-09-30 01:34:22 -0400

    Add XXXModelSelector module to prettier ignore

--- tests/lib/abstractTypes/models/OwnerModelSelector.js
+++ tests/lib/abstractTypes/models/OwnerModelSelector.js
@@ -3,22 +3,10 @@ import { OrganizationModelSelector } from "./OrganizationModel.base"
 import { UserModelSelector } from "./UserModel.base"
 
 export class OwnerModelSelector extends QueryBuilder {
-  get id() {
-    return this.__attr(`id`)
-  }
-  get name() {
-    return this.__attr(`name`)
-  }
-  user(builder) {
-    return this.__inlineFragment(`User`, UserModelSelector, builder)
-  }
-  organization(builder) {
-    return this.__inlineFragment(
-      `Organization`,
-      OrganizationModelSelector,
-      builder
-    )
-  }
+  get id() { return this.__attr(`id`) }
+  get name() { return this.__attr(`name`) }
+  user(builder) { return this.__inlineFragment(`User`, UserModelSelector, builder) }
+  organization(builder) { return this.__inlineFragment(`Organization`, OrganizationModelSelector, builder) }
 }
 export function selectFromOwner() {
   return new OwnerModelSelector()
