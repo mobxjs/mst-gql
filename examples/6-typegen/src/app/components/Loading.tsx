b3a680cb7959734ca2ee0f96b5ae92824132c9f6
Chris Drackett <chrisdrackett@users.noreply.github.com>
2019-10-28 13:52:41 -0700

    remove example 6

--- examples/6-typegen/src/app/components/Loading.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react"
-
-export const Loading = () => <p>Loading...</p>
--- examples/6-typegen/src/app/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* This is a mst-gql generated file, don't modify it manually */
-/* eslint-disable */
-/* tslint:disable */
-
-export * from "./TodoModel"
-export * from "./UserModel"
-export * from "./RootStore"
-export * from "./reactUtils"
