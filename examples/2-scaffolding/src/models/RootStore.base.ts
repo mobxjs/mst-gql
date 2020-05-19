9f51c00c1d77dea555dad53c9241dd10b5270a80
Brandon <brobertstech@gmail.com>
2020-05-18 18:17:18 -0700

    Run example 2

--- examples/2-scaffolding/src/models/RootStore.base.ts
+++ examples/2-scaffolding/src/models/RootStore.base.ts
@@ -23,6 +23,22 @@ type Refs = {
   attacks: ObservableMap<string, AttackModelType>
 }
 
+
+/**
+* Enums for the names of base graphql actions
+*/
+
+export enum RootStoreBaseQueries {
+queryQuery="queryQuery",
+queryPokemons="queryPokemons",
+queryPokemon="queryPokemon"
+}
+export enum RootStoreBaseMutations {
+mutateQuery="mutateQuery",
+mutatePokemons="mutatePokemons",
+mutatePokemon="mutatePokemon"
+}
+
 /**
 * Store, managing, among others, all the objects received through graphQL
 */
