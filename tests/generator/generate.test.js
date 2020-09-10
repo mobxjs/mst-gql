5b6ecd1eb42e77268846c92ff174e79e831a498c
Balazs E. Pataki <pataki@gmail.com>
2020-09-10 16:35:55 +0200

    Don't generate selectors for scalar types
    This fixes issue #252

--- tests/generator/generate.test.js
+++ tests/generator/generate.test.js
@@ -420,10 +420,27 @@ type HelloResult {
     hasFileContentRegexp(rootStoreBase, "import { .*HelloResultModelSelector }")
   ).toBeTruthy()
 
+  // This should be generated for the query with scalar (String) return type
+  expect(
+    hasFileContentExact(
+      rootStoreBase,
+      "queryHelloWithString(variables?: {  }, options: QueryOptions = {}) {"
+    )
+  ).toBeTruthy()
+
+  // This should be generated for the query with complex return type
+  expect(
+    hasFileContentExact(
+      rootStoreBase,
+      "queryHelloWithType(variables?: {  }, resultSelector: string | ((qb: HelloResultModelSelector) => HelloResultModelSelector) = helloResultModelPrimitives.toString(), options: QueryOptions = {})"
+    )
+  ).toBeTruthy()
+
+  // Make sure no StringModelSelector is generated
   expect(
     hasFileContentRegexp(rootStoreBase, ".*StringModelSelector")
-  ).toBeTruthy()
+  ).toBeFalsy()
   expect(
     hasFileContentRegexp(rootStoreBase, "import { .*StringModelSelector }")
-  ).toBeTruthy()
+  ).toBeFalsy()
 })
