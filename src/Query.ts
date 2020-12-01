8b9192fea5e19d04b065b87f78737f3e12881822
Braden Mars <bradenmars@bradenmars.me>
2020-11-30 18:59:05 -0600

    fix(ts): type error TS7006 in query

--- src/Query.ts
+++ src/Query.ts
@@ -140,7 +140,7 @@ export class Query<T = unknown> implements PromiseLike<T> {
         this.error = false
         this.data = data
       }),
-      action((error) => {
+      action((error: any) => {
         this.loading = false
         this.error = error
       })
