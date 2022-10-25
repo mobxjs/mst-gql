2ecc1157bb3edfe8cdbac9528fe3db7aaf62bbf2
Dmytro Shpak <dshpak@studentcoin.org>
2022-10-25 22:03:01 +0200

    feat: support of non standard id field name

--- src/deflateHelper.ts
+++ src/deflateHelper.ts
@@ -3,11 +3,14 @@ export function deflateHelper(store: any, data: any) {
     if (!data || typeof data !== "object") return data
     if (Array.isArray(data)) return data.map(deflate)
 
-    const { __typename, id } = data
+    const { __typename } = data
+
 
     if (__typename && store.isRootType(__typename)) {
-      // GQL object with root type, keep only __typename & id
-      return { __typename, id }
+      // GQL object with root type, keep only __typename & identifier
+      const typeDef = store.getTypeDef(__typename)
+      const idName = typeDef.identifierAttribute ?? 'id'
+      return { __typename, [idName]: data[idName] }
     } else {
       // GQL object with non-root type, return object with all props deflated
       const snapshot: any = {}
--- src/mergeHelper.ts
+++ src/mergeHelper.ts
@@ -6,7 +6,7 @@ export function mergeHelper(store: any, data: any) {
     if (!data || typeof data !== "object") return data
     if (Array.isArray(data)) return data.map(merge)
 
-    const { __typename, id } = data
+    const { __typename } = data
 
     // convert values deeply first to MST objects as much as possible
     const snapshot: any = {}
@@ -18,6 +18,9 @@ export function mergeHelper(store: any, data: any) {
     if (__typename && store.isKnownType(__typename)) {
       // GQL object with known type, instantiate or recycle MST object
       const typeDef = store.getTypeDef(__typename)
+
+      const id = data[typeDef.identifierAttribute ?? 'id']
+
       // Try to reuse instance, even if it is not a root type
       let instance = id !== undefined && resolveIdentifier(typeDef, store, id)
       if (instance) {
